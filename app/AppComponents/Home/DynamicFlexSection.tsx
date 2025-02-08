f8ba47f3aad6170d172c3074922818737a965961
Rajdeep Ghosh <rajdeep.ghosh@fedev.cbnits.com>
2025-02-09 01:01:52 +0530

    commit - 8

--- app/AppComponents/Home/DynamicFlexSection.tsx
+++ app/AppComponents/Home/DynamicFlexSection.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 type Item = {
@@ -8,21 +10,28 @@ type Item = {
 };
 
 const DynamicFlexSection: React.FC<{ items: Item[] }> = ({ items }) => {
-  const isMobile = window.innerWidth <= 640
-  const isTab = window.innerWidth >= 640 && window.innerWidth <= 1024
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isMobile = windowWidth <= 640;
+  const isTab = windowWidth >= 640 && windowWidth <= 1024;
   const maxPerRow = isMobile ? 2 : isTab ? 3 : 6;
+
   const remainingItems = [...items];
   let rowSize = maxPerRow;
   const rows: Item[][] = [];
 
-  console.log("isTab :: ", isTab)
-
-  // Generate rows in decreasing order
   while (remainingItems.length > 0) {
-    console.log('rowSize :: ', rowSize)
     const row = remainingItems.splice(0, rowSize);
     rows.push(row);
-    rowSize = isMobile ? 2 : isTab ? 3 : Math.max(1, rowSize - 1); // Ensure at least 1 item per row
+    rowSize = isMobile ? 2 : isTab ? 3 : Math.max(1, rowSize - 1);
   }
 
   return (
@@ -36,9 +45,7 @@ const DynamicFlexSection: React.FC<{ items: Item[] }> = ({ items }) => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, ease: "easeOut" }}
             >
-              <div
-                className="bg-black px-6 py-3 shadow-md shadow-zinc-400  text-white flex items-center justify-center gap-x-3 text-sm lg:text-2xl font-semibold rounded-2xl border-t-[1px] border-x-[1px] border-t-white/60 border-x-white/60"
-              >
+              <div className="bg-black px-6 py-3 shadow-md shadow-zinc-400 text-white flex items-center justify-center gap-x-3 text-sm lg:text-2xl font-semibold rounded-2xl border-t-[1px] border-x-[1px] border-t-white/60 border-x-white/60">
                 {item.icon}
                 {item.label}
               </div>
