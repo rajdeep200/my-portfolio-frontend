d6da2f2a1b0e89365b418289f1d3f717d748b29f
Rajdeep Ghosh <rajdeep.ghosh@fedev.cbnits.com>
2025-06-08 00:07:07 +0530

    Fixed project section design issue

--- app/AppComponents/Home/ProjectCard.tsx
+++ app/AppComponents/Home/ProjectCard.tsx
@@ -18,12 +18,12 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
     <div className="flex flex-col justify-center items-center">
       <Card className="w-[80%] h-full mx-auto mt-4 lg:mt-[8rem] xl:mt-[5.5rem] xl:mx-0 relative">
-        <CardHeader className="flex justify-center items-center min-h-[150px]">
+        <CardHeader className="flex justify-center items-center min-h-[120px] lg:min-h-[150px]">
           {project.img}
         </CardHeader>
         <CardContent className="mx-auto flex flex-col justify-between pb-[6rem]">
           <div className="flex flex-col gap-y-5 justify-center items-center">
-            <p className="font-poppins text-center text-[15px] leading-[1.5em] text-zinc-500 font-medium">
+            <p className="font-poppins text-center text-[12px] lg:text-[15px] leading-[1.5em] text-zinc-500 font-medium">
               {project.description}
             </p>
           </div>
