150a959aaa230756ff22751ab074bc3c05061fda
Rajdeep Ghosh <rajdeep.ghosh@fedev.cbnits.com>
2025-06-07 20:00:29 +0530

    Added Calendly

--- app/AppComponents/ContactSection.tsx
+++ app/AppComponents/ContactSection.tsx
@@ -8,49 +8,66 @@ import { Button } from "@/components/ui/button";
 import { sendQuery } from "../services/contactService";
 import { ToastContainer, toast } from "react-toastify";
 import { Loader } from "./Loader";
+import { InlineWidget } from "react-calendly";
 
 const ContactSection = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async () => {
     try {
       setLoading(true);
-      if(!name.length || !email.length || !message.length){
-        toast.error("Please fill all the fields")
-        return
+      if (!name.length || !email.length || !message.length) {
+        toast.error("Please fill all the fields");
+        return;
       }
       const response = await sendQuery({ name, email, message });
       if (response.ack) {
-        toast.success("Message sent successfully!")
+        toast.success("Message sent successfully!");
         setName("");
         setEmail("");
         setMessage("");
       } else {
-        toast.error("Oh snap! Please try again.")
+        toast.error("Oh snap! Please try again.");
       }
     } catch (error) {
-      toast.error("Something went wrong...")
+      toast.error("Something went wrong...");
       console.error(error);
-    }finally{
-      setLoading(false)
+    } finally {
+      setLoading(false);
     }
   };
+
+  const el = document.getElementById("__next");
+  console.log("element", el);
   return (
     <>
       <ToastContainer />
-      <div id="contact-section" className="flex flex-col justify-center items-center">
+      <div
+        id="contact-section"
+        className="flex flex-col justify-center items-center"
+      >
         <PrimaryHeading
           textCentered
           firstTitle="LET's HAVE"
           secondTitle="A CHAT"
         />
+        <InlineWidget
+          styles={{
+            marginTop: "50px",
+            overflow: "hidden",
+            height: "250px",
+            backgroundColor: "black",
+            border: "none",
+            borderRadius: "10px",
+            boxShadow: "0px 0px 30px white",
+          }}
+          url="https://calendly.com/grajdeep2000"
+        />
         <div className="relative font-poppins shadow-md shadow-zinc-400 mt-[8%] mb-[8%] flex flex-col justify-center items-center gap-y-6 px-3 pt-3 pb-6 rounded-xl bg-zinc-900 text-white md:mx-auto w-[80%] md:w-[70%] lg:w-[50%] xl:w-[40%]">
-          {
-            loading && <Loader/>
-          }
+          {loading && <Loader />}
           <Input
             value={name}
             onChange={(e) => setName(e.target.value)}
